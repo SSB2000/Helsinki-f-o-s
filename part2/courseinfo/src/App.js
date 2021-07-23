fb8d751514baf8630dcaa211b9b8aaf4bb355646
shubham singh bohra <shubhambohra292@gmail.com>
2021-07-23 09:49:08 +0530

    Refractored to using axios

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,64 +1,68 @@
-import { useState } from 'react'
-import React from 'react'
-import Note from './Components/Note' // For exercise 2.6 - 2.10
+import React, { useState, useEffect } from "react";
+import Note from "./Components/Note"; // For exercise 2.6 - 2.10
 // import { date } from 'check-types';
 // import Course from './Components/Course' // For exercise 2.1 - 2.5
-
+import axios from "axios";
 
 // Exercise 2.6 - 2.10
-const App = ( props ) => {
-  const [ notes, setNotes ] = useState(props.notes);
-  const [ newNote, setNewNote ] = useState("");
-  const [ showAll, setShowAll ] = useState(true);
+const App = () => {
+    const [notes, setNotes] = useState([]);
+    const [newNote, setNewNote] = useState("");
+    const [showAll, setShowAll] = useState(true);
 
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important);
-
-  const addNote = (event) => {
-    event.preventDefault();
-    const noteObject = {
-      id: notes.length + 1,
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random > 0.5,
+    const hook = () => {
+        axios.get("http://localhost:3001/notes").then((response) => {
+            setNotes(response.data);
+        });
     };
-    
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
-  }
 
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value);
-  }
+	useEffect(hook, []);
 
-  return (
-    <div>
-      <h1>Notes</h1>
-      <div>
-        <button 
-          type="button" 
-          onClick={() => setShowAll(!showAll)}
-        >
-          {showAll ? "important" : "all"}
-        </button>
-      </div>
-      <ul>
-        {notesToShow.map(note => 
-            <Note key={note.id} note={note} />
-        )}
-      </ul>
-      <form onSubmit={addNote}>
-        <input 
-          placeholder="Enter your note here..."
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">Save</button>
-      </form>
-    </div>
-  )
-}
+    const notesToShow = showAll
+        ? notes
+        : notes.filter((note) => note.important);
+
+    const addNote = (event) => {
+        event.preventDefault();
+        const noteObject = {
+            id: notes.length + 1,
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random > 0.5,
+        };
+
+        setNotes(notes.concat(noteObject));
+        setNewNote("");
+    };
+
+    const handleNoteChange = (event) => {
+        setNewNote(event.target.value);
+    };
+
+    return (
+        <div>
+            <h1>Notes</h1>
+            <div>
+                <button type="button" onClick={() => setShowAll(!showAll)}>
+                    {showAll ? "important" : "all"}
+                </button>
+            </div>
+            <ul>
+                {notesToShow.map((note) => (
+                    <Note key={note.id} note={note} />
+                ))}
+            </ul>
+            <form onSubmit={addNote}>
+                <input
+                    placeholder="Enter your note here..."
+                    value={newNote}
+                    onChange={handleNoteChange}
+                />
+                <button type="submit">Save</button>
+            </form>
+        </div>
+    );
+};
 
 // Exercies 2.1 - 2.5
 /*
@@ -118,7 +122,4 @@ const App = () => {
 }
 */
 
-
-
-
-export default App
+export default App;
