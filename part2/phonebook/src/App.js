70e425f61224ecae864f21b00ca0ad4e8687c532
shubham singh bohra <shubhambohra292@gmail.com>
2021-07-28 11:00:26 +0530

    Complete phonebook step9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import PersonInfo from "./Components/PersonInfo";
 import PersonForm from "./Components/PersonForm";
-import axios from 'axios'
+import personService from "./services/person";
+import axios from "axios";
+import person from "./services/person";
 // import Search from "./Components/Search";
 const App = () => {
     const [persons, setPersons] = useState([]);
@@ -9,9 +11,9 @@ const App = () => {
     const [newNumber, setNewNumber] = useState("");
 
     const hook = () => {
-        axios.get('http://localhost:3001/persons').then(response => {
-            setPersons(response.data);
-        });
+        personService
+            .getAll()
+            .then(initialPersons => setPersons(initialPersons));
     }
 
     useEffect(hook, [])
@@ -27,35 +29,56 @@ const App = () => {
             number: newNumber,
         };
 
-        setPersons(persons.concat(newPersonObject));
+        personService
+            .create(newPersonObject)
+            .then(returnedPerson => {
+                setPersons(persons.concat(returnedPerson))
+            });
+
+        // setPersons(persons.concat(newPersonObject));
         setNewName("");
         setNewNumber("");
     };
 
     const handleNameAddition = (event) => {
         const name = event.target.value;
-        console.log(`name`, name);
         setNewName(name);
     };
 
     const handleNumberAddition = (event) => {
         setNewNumber(event.target.value);
     };
+
+    const handleDelete = (id) => {
+        const toDelete = persons.find(person => person.id === id);
+        if (window.confirm(`Delete ${toDelete.name} ?`)) {
+            personService
+            .deletePerson(id)
+            .then(() => {
+                setPersons(persons.filter(person => person.id !== id))
+            });
+        }
+    }
+
     return (
         <div>
             <h2>Phonebook</h2>
             {/* <Search persons={persons} /> */}
             <PersonForm
-                addPerson={addPerson}
                 newName={newName}
                 handleNameAddition={handleNameAddition}
                 newNumber={newNumber}
                 handleNumberAddition={handleNumberAddition}
+                addPerson={addPerson}
             />
             <h2>Numbers</h2>
             <ul>
                 {persons.map((person) => (
-                    <PersonInfo person={person} />
+                    <PersonInfo 
+                        key={person.id} 
+                        person={person}
+                        handleDelete={handleDelete}
+                    />
                 ))}
             </ul>
         </div>
--- part2/phonebook/src/Components/PersonInfo.js
+++ part2/phonebook/src/Components/PersonInfo.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-export default function PersonInfo({ person }) {
+export default function PersonInfo({ person, handleDelete }) {
     return (
         <li>
             {person.name}: {person.number}
+            <button 
+                type="submit"
+                onClick={() => handleDelete(person.id)}
+            >
+            Delete
+            </button>
         </li>
     );
 }
